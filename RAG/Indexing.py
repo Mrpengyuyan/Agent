57ea90bf44bd882e60eddc31b5954e9d824f8b3d
Mrpengyuyan <Mrpengyuyan@users.noreply.github.com>
2026-04-09 00:07:50 +0800

    Update RAG indexing with Chroma embeddings

--- RAG/Indexing.py
+++ RAG/Indexing.py
@@ -1,5 +1,15 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+
+embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+vector_store = Chroma(
+    collection_name="example_collection",
+    embedding_function=embeddings,
+    persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
+)
 
 loader = PyPDFLoader("/Users/maochuandou/Internship/Agent/RAG/窦茂川简历.pdf")
 # loader = DirectoryLoader("./文件夹/"，glob = “**/*.pdf”, loader_cls = PyPDFLoader)
@@ -17,4 +27,12 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 all_splits = text_splitter.split_documents(docs)
 
-print(f"Split blog post into {len(all_splits)} sub-documents.")
+# print(f"Split blog post into {len(all_splits)} sub-documents.")
+
+# print(type(docs))
+# print(type(docs[0]))
+
+
+document_ids = vector_store.add_documents(documents=all_splits)
+
+print(document_ids[:3])
