629c40ba44ace38cf6fe1d487aa0bc5cd52380ff
Mrpengyuyan <2621733080@qq.com>
2026-04-12 23:02:34 +0800

    Update llama tools demo

--- agentDemo/tools_llama.py
+++ agentDemo/tools_llama.py
@@ -1,5 +1,8 @@
 from llama_index.core.tools import FunctionTool
 from llama_index.llms.ollama import Ollama
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import VectorStoreIndex
 
 
 def add(x: int, y: int) -> int:
@@ -22,3 +25,15 @@ response = llm.predict_and_call(
     verbose=True,
 )
 print(str(response))
+
+
+# load documents
+documents = SimpleDirectoryReader(input_files=["./agentDemo/窦茂川简历.pdf"]).load_data()
+
+splitter = SentenceSplitter(chunk_size=1024)
+nodes = splitter.get_nodes_from_documents(documents)
+
+print(nodes[0].get_content(resumedata_mode="all"))
+
+vector_index = VectorStoreIndex(nodes)
+query_engine = vector_index.as_query_engine(similarity_top_k=2)
